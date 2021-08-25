fb47a07bc8dc7046f391a642aabcd681016c23e0
Davi <davi.freitas@useall.com.br>
2021-08-25 16:57:17 -0300

    ts-node-dev setup

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello world" });
+});
+
 app.listen(3333, () => console.log("Server is running at por 3333"));
