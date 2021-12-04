96893edb4c3d748838887650204958146355f81c
UbDavi <davi.frrs@outlook.com>
2021-12-04 12:48:49 -0300

    Adapted categories class to create in the database

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,6 +4,8 @@ interface IOptions {
   host: string;
 }
 
+('Arquivo database');
+
 getConnectionOptions().then(options => {
   const newOptions = options as IOptions;
   newOptions.host = 'database'; // Essa opção deve ser EXATAMENTE o nome dado ao service do banco de dados
